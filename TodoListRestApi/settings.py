7ef380ec34de8dac8758c114ef019d7e2b80e61c
Mesut Canpolat <mesutcanpolat06@icloud.com>
2022-04-17 21:12:05 +0300

    Yeni todo appi eklendi
    Yeni todo appi eklendi

--- TodoListRestApi/settings.py
+++ TodoListRestApi/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+    'rest_framework',
+    'todos'
+    ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -76,8 +78,11 @@ WSGI_APPLICATION = 'TodoListRestApi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ToDoListDb',
+        'USER': 'postgres',
+        'PASSWORD': '123456',
+        'HOST': 'localhost'
     }
 }
 
